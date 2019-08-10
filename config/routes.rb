aa85a12aecbc10f3ae229ffba9f2605cdc8f5f89
kazuki mokuo <sp9t5ns9@icloud.com>
2019-08-10 14:08:28 +0900

    グループ作成機能へのビューの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "messages#index"
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:edit, :update, :new, :create] do
-    resources :messages, only: [:index, :create]
-  end  
+  resources :groups, only: [:edit, :update, :new, :create] 
+  
 end
