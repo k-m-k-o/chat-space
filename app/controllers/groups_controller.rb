1ec091277bdf142dfd533063a3b5434cc281ba25
kazuki mokuo <sp9t5ns9@icloud.com>
2019-08-10 13:45:54 +0900

    グループモデル、コントローラーの追加

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,24 @@
+class GroupsController < ApplicationController
+  def new
+
+  end
+  
+  def create
+
+  end
+  
+  def edit
+
+  end
+  
+  def show
+
+  end  
+
+  private
+
+  def group_params
+
+  end  
+
+end
