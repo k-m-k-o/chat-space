779beb4d361497bd46d77cb157c1d2d95d54f5a8
kazuki mokuo <sp9t5ns9@icloud.com>
2019-08-10 12:20:20 +0900

    ユーザー登録、編集機能の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :permit_devise_name, if: :devise_controller?
+  protected
+  
+  def permit_devise_name
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end  
 end
