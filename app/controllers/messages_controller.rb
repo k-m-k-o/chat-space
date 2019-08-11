397181b7ed006d41b9e63b80ed0a2b0eff1a1854
kazuki mokuo <sp9t5ns9@icloud.com>
2019-08-11 11:39:29 +0900

    ルーティングの追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,15 @@ class MessagesController < ApplicationController
   def index
 
   end  
+
+  def create
+
+  end  
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:id])
+  end
+
 end
