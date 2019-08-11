edf4908b2781f95c4b6a28d6b8577bc41fd7ba8a
kazuki mokuo <sp9t5ns9@icloud.com>
2019-08-11 15:45:02 +0900

    メッセージ機能の実装
    画像のサイズ以外のメッセージ投稿機能、メッセージ表示機能、

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,19 @@ class Group < ApplicationRecord
   has_many :users,through: :members
   has_many :members
   has_many :messages
+
+  def show_group_users
+    text = "Member: "
+    users.each do |user|
+      text += user.name + " "
+    end
+    return text
+  end  
+  def show_last_messages
+    if (last_message = messages.last).present?
+      last_message.body? ? last_message.body : "画像が投稿されています"
+    else
+      "まだメッセージはありません"
+    end
+  end  
 end
